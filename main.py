4ea90f19c4ed0499a33e7a8f93c563c364504165
Logan Montalto <64017390+Logan-Montalto@users.noreply.github.com>
2020-11-16 17:04:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,46 +8,50 @@ from donnees import creation_grille
 import time
 import keyboard
 
-if __name__=='__main__':
-    grille = creation_grille()
-    touche = 0
+grille = creation_grille()
+timer = []
 
-    #Affichage de la grille
-    def affichage(g):
+
+"""Affichage de la grille"""
+
+def affichage(g):
+    print()
+    list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    print("   0 1 2 3 4 5 6 7 8 9")
+    for y in range(10):
+        print(list[y], "|", end='')
+        for x in range(10):
+            if g[y][x] == 10:
+                print('o', end='|')
+            elif g[y][x] % 10 == 0 and g[y][x] != 0:
+                print('o', end='|')
+            elif g[y][x] > 10 and g[y][x] % 10 != 0:
+                print('x', end='|')
+            else:
+                print('.', end='|')
         print()
-        list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-        print("   0 1 2 3 4 5 6 7 8 9")
-        for y in range(10):
-            print(list[y], "|", end='')
-            for x in range(10):
-                if g[y][x] == 10:
-                    print('o', end='|')
-                elif g[y][x] % 10 == 0 and g[y][x] != 0:
-                    print('o', end='|')
-                elif g[y][x] > 10 and g[y][x] % 10 != 0:
-                    print('x', end='|')
-                else:
-                    print('.', end='|')
-            print()
 
-    #Introdution au jeu
+
+# Introdution au jeu
+def init():
     nom = input("Saisir votre nom : ")
     print()
     print("Bienvenue", nom + '!')
     print("Le but est de couler les 4 bateaux le plus rapidement possible. Bonne Chance !")
     print()
     print("Tapez la touche 'Ctrl' pour commencer la partie : ")
-    rk = keyboard.record(until ='Ctrl')
-    keyboard.play(rk, speed_factor = 1)
+    rk = keyboard.record(until='Ctrl')
+    keyboard.play(rk, speed_factor=1)
 
-    #Début du timer
-    t1 = time.time()
+    temps()
 
+    touche = 0
     while touche != 14:
         affichage(grille)
         print()
         chaine = input("Quel est ton coup " + nom + ' ?')
-        while len(chaine) != 2 or chaine[0] < 'A' or 'K' <= chaine[0] < 'a' or 'k' <= chaine[0] or chaine[1] < '0' or chaine[1] > '9':
+        while len(chaine) != 2 or chaine[0] < 'A' or 'K' <= chaine[0] < 'a' or 'k' <= chaine[0] or chaine[1] < '0' or \
+                chaine[1] > '9':
             print("Veuillez entrer une lettre et un chiffre")
             chaine = input("Quel est ton coup " + nom + ' ?')
         print("Tu as joué", chaine[0].upper() + chaine[1])
@@ -63,12 +67,31 @@ if __name__=='__main__':
     affichage(grille)
     print()
 
-    #Fontion de fin de jeu contenant le 2ème timer
-    #Affichage du timer
-    def fin_jeu():
-        print("Bien joué, tu as coulé tous les bateaux !")
-        t2 = time.time()
-        t = t2-t1
-        z = round(t,2)
-        print("Tu as mis " + str(z) + " secondes.")
+
+"""
+Fonction commençant le timer (t1)
+"""
+
+def temps():
+    t1 = time.time()
+    timer.append(t1)
+
+
+"""
+Fontion de fin de jeu contenant le 2ème timer
+Affiche le timer final 
+timer final = (temps .time() au début du jeu) - (temps .time() au moment de la fin du jeu)
+"""
+
+def fin_jeu():
+    print("Bien joué, tu as coulé tous les bateaux !")
+    t2 = time.time()
+    t = t2 - timer[0]
+    z = round(t, 2)
+    print("Tu as mis " + str(z) + " secondes.")
+
+
+if __name__ == '__main__':
+    init()
+    affichage(grille)
     fin_jeu()
