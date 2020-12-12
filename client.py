0da5fde2da966234e86d98090dd6ca2a680df9c1
Logan-Montalto <He201795@students.ephec.be>
2020-12-12 10:30:07 +0100

    modif serveur et client

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import pickle
+import threading
+
+
+def main():
+    port = 5050
+    recup_ip = socket.gethostbyname(socket.gethostname())
+    adresse = (recup_ip, port)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = IPV4
+    client.connect(adresse) #connecter le client au serveur
+    return client
+
+
+def gestion_objet():
+    c = main()
+    envoie = input("Envoie quelque chose que tu veux : ")
+    c.send(pickle.dumps(envoie))  # dumps = compression
+    donnees = c.recv(2048)
+    msg_recu = pickle.loads(donnees)  # déchiffrer les données, décompression des données
+    print(msg_recu)
+    gestion_objet()
+
+if __name__ == '__main__':
+    gestion_objet()
--- serveur.py
+++ serveur.py
@@ -8,7 +8,7 @@ def main():
     recup_ip = socket.gethostbyname(socket.gethostname())
     print(f"Le serveur écoute sur {recup_ip}")
     adresse = (recup_ip, port)
-    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPV4
+    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = IPV4
     serveur.bind(adresse) #lier le serveur avec l'adresse
     return serveur
 
@@ -18,7 +18,29 @@ def start_serveur():
     print("Le serveur démarre")
     s.listen()
     while True:
-        conn, adresse_client = s.accept()
-        thread_gestion = threading.Thread(target=gestion_client, args=(conn, adresse_client))
+        connexion, adresse_client = s.accept()
+        thread_gestion = threading.Thread(target=gestion_client, args=(connexion, adresse_client))
         thread_gestion.start()
         print(f"Connexion active {threading.activeCount()-1}")
+
+
+def gestion_client(connexion, adresse_client):
+    print(f"Adresse du client = {adresse_client}")
+    connect = True
+    while connect:
+        try:
+            donnees = connexion.recv(2048)
+            reception = pickle.loads(donnees) #déchiffrer les données, décompression des données
+            print(reception)
+            if not donnees:
+                print("Mince, je suis déconnecté")
+                connect = False
+            envoie = input("Envoie quelque chose que tu veux : ")
+            connexion.send(pickle.dumps(envoie)) #dumps = compression
+        except:
+            print("client déconnecté")
+            connect = False
+    connexion.close()
+
+if __name__ == '__main__':
+    start_serveur()
