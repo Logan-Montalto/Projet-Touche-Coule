fd3b649e263ccac122c61e76eea1b658419f2af5
Logan-Montalto <He201795@students.ephec.be>
2020-12-12 09:52:33 +0100

    modif serveur

--- serveur.py
+++ serveur.py
@@ -1,14 +1,24 @@
 import socket
 import pickle
+import threading
+
 
 def main():
     port = 5050
     recup_ip = socket.gethostbyname(socket.gethostname())
     print(f"Le serveur écoute sur {recup_ip}")
-    addresse = (recup_ip, port)
+    adresse = (recup_ip, port)
     serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPV4
-    serveur.bind(addresse) #lier le serveur avec l'adresse
+    serveur.bind(adresse) #lier le serveur avec l'adresse
     return serveur
+
 
 def start_serveur():
     s = main()
+    print("Le serveur démarre")
+    s.listen()
+    while True:
+        conn, adresse_client = s.accept()
+        thread_gestion = threading.Thread(target=gestion_client, args=(conn, adresse_client))
+        thread_gestion.start()
+        print(f"Connexion active {threading.activeCount()-1}")
