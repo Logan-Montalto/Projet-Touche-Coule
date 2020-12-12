b5d5095c323f5154517a314bb593f1f8c43271ac
Logan-Montalto <He201795@students.ephec.be>
2020-12-12 09:44:35 +0100

    serveur

--- serveur.py
+++ serveur.py
@@ -5,4 +5,10 @@ def main():
     port = 5050
     recup_ip = socket.gethostbyname(socket.gethostname())
     print(f"Le serveur écoute sur {recup_ip}")
-    
+    addresse = (recup_ip, port)
+    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPV4
+    serveur.bind(addresse) #lier le serveur avec l'adresse
+    return serveur
+
+def start_serveur():
+    s = main()
