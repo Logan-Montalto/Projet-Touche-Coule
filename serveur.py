fc970ffddbf92e3d25051177a39d484066bbd2b9
Logan-Montalto <He201795@students.ephec.be>
2020-12-17 18:45:59 +0100

    Update serveur.py
    - Update main()
    - Update start_serveur()
    - Update gestion_client()

--- serveur.py
+++ serveur.py
@@ -2,79 +2,95 @@ import socket
 import pickle
 import threading
 import grille_auto
-
+from class_multi import Multi
 
 creation = grille_auto.CreationTableauAuto()
 tableau_client = []
+tableau_nom = []
 
 
 def main():
+    """
+
+    :return:
+    """
     port = 5050
     recup_ip = socket.gethostbyname(socket.gethostname())
     print(f"Le serveur écoute sur {recup_ip}")
     adresse = (recup_ip, port)
-    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = IPV4
-    serveur.bind(adresse) #lier le serveur avec l'adresse
+    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IPV4
+    serveur.bind(adresse)  # lier le serveur avec l'adresse
     return serveur
 
 
 def start_serveur():
-    creation.creation_grille()
+    """
+
+    :return:
+    """
     s = main()
     print("Le serveur démarre")
+    creation.creation_grille()
     s.listen()
     while True:
         connexion, adresse_client = s.accept()
-        thread_gestion = threading.Thread(target=gestion_client, args=(connexion, adresse_client, creation.recup_grille()))
+        thread_gestion = threading.Thread(target=gestion_client,
+                                          args=(connexion, adresse_client, creation.recup_grille()))
         thread_gestion.start()
-        print(f"Connexion active {threading.activeCount()-1}")
+        print(f"Connexion active {threading.activeCount() - 1}")
 
 
 def gestion_client(connexion, adresse_client, tableau):
+    """
+
+    :param connexion:
+    :param adresse_client:
+    :param tableau:
+    :return:
+    """
     print("Mon tableau")
-    print(affichage(tableau))
+    current_player = Multi(tableau)
+    print(current_player.affichage_bateau())
     print(f"Adresse du client = {adresse_client}")
     connect = True
     compteur = 0
     while connect:
         try:
             donnees = connexion.recv(2048)
-            reception = pickle.loads(donnees) #déchiffrer les données, décompression des données
+            reception = pickle.loads(donnees)  # déchiffrer les données, décompression des données
+            if reception == "Il a gagné !":
+                print(tableau_nom[0] + " a gagné !")
+                connect = False
             if compteur == 0:
                 tableau_client.append(reception)
+            if compteur == 1:
+                tableau_nom.append(reception)
             print(reception)
-            print(affichage(tableau_client[0]))
+            other_player = Multi(tableau_client[0])
+            print(other_player.affichage_cache())
             if not donnees:
                 print("Mince, je suis déconnecté")
                 connect = False
             if compteur == 0:
-                connexion.send(pickle.dumps(tableau)) #dumps = compression
+                connexion.send(pickle.dumps(tableau))  # dumps = compression
                 compteur += 1
             elif compteur == 1:
                 envoie_nom = input("Quel est ton pseudo ? : ")
+                tableau_nom.append(envoie_nom)
                 connexion.send(pickle.dumps(envoie_nom))  # dumps = compression
                 compteur += 1
             else:
-                envoie = input("Envoie quelque chose que tu veux : ")
-                connexion.send(pickle.dumps(envoie))  # dumps = compression
+                envoie = input("Quel est ton coup " + tableau_nom[1] + ' ?')
+                while not other_player.initialisation_jeu(tableau_nom[1], envoie):
+                    envoie = input("Quel est ton coup " + tableau_nom[1] + ' ?')
+                if other_player.winner():
+                    envoie = "Il a gagné !"
+                    connexion.send(pickle.dumps(envoie))  # dumps = compression
+                    connect = False
+                else:
+                    connexion.send(pickle.dumps(envoie))  # dumps = compression
+                    print(other_player.affichage_cache())
         except:
             print("client déconnecté")
             connect = False
     connexion.close()
-
-def affichage(tableau):
-    print()
-    list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    print("   0 1 2 3 4 5 6 7 8 9")
-    for y in range(10):
-        print(list[y], "|", end='')
-        for x in range(10):
-            if tableau[y][x] == 10:
-                print('o', end='|')
-            elif tableau[y][x] % 10 == 0 and tableau[y][x] != 0:
-                print('o', end='|')
-            elif tableau[y][x] > 10 and tableau[y][x] % 10 != 0:
-                print('x', end='|')
-            else:
-                print(str(tableau[y][x]), end='|')
-        print()
